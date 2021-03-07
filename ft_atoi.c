1d404020f67febe1d9831fa4803d8d7204fe18a6
Mariana Lima <mariana.m.lima97@hotmail.com>
2021-03-07 17:44:14 -0300

    teste

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mmanoel- <mmanoel-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 20:52:52 by mmanoel-          #+#    #+#             */
-/*   Updated: 2021/03/07 17:10:13 by mmanoel-         ###   ########.fr       */
+/*   Updated: 2021/03/07 17:44:05 by mmanoel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,10 @@
 
 int					ft_atoi(const char *str)
 {
-	int				val;
 	unsigned int	add;
-	int				mult;
-	int			isneg;
+	int				isneg;
 
 	add = 0;
-	mult = 1;
 	isneg = 1;
 	while (*str != '\0' && (*str == ' ' || *str == '\t' || *str == '\n' ||
 								*str == '\f' || *str == '\r' || *str == '\v'))
