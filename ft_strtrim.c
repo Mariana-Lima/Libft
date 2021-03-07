389c3feec2fc1852e05ad3365148817fcea0e252
Mariana Lima <mariana.m.lima97@hotmail.com>
2021-03-07 01:11:04 -0300

    teste

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,33 +1,5 @@
 #include "libft.h"
 
-char  *ft_substr(char const *s, unsigned int start, size_t len)
-{
-	size_t	c;
-	size_t	cc;
-	char	*substr;
-
-	c = 0;
-	cc = 0;
-	
-    substr = (char *)malloc(sizeof(char) * (len + 1));
-  	if (substr == NULL)
-		return (NULL);  
-    substr[len] = '\0';
-
-
-	while (s[c] != '\0')
-	{
-		if (c >= start && cc < len)
-		{
-			substr[cc] = s[c];
-			cc++;
-		}
-		c++;
-	}
-
-	return (substr);
-}
-
 int compare( char s1, char const *set)
 {
     int c;
