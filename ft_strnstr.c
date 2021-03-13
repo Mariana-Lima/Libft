dec963c19c5ed3231530bc7841cf19db2d8ee9ca
Mariana Lima <mariana.m.lima97@hotmail.com>
2021-03-13 15:50:34 -0300

    Tentativa1

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mmanoel- <mmanoel-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/07 19:21:24 by mmanoel-          #+#    #+#             */
-/*   Updated: 2021/03/07 19:43:35 by mmanoel-         ###   ########.fr       */
+/*   Updated: 2021/03/13 15:50:29 by mmanoel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ char		*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
 	size_t	len_len;
 
-	if (*s2 == '\0')
-		return ((char *)s1);
 	len_len = ft_strlen(s2);
 	while (len-- >= len_len && *s1 != '\0')
 	{
