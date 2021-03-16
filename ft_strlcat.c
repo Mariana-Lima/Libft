d971e24646759a2bd896abf4e10502accf8961c4
Mariana Lima <mariana.m.lima97@hotmail.com>
2021-03-15 22:17:45 -0300

    final

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mmanoel- <mmanoel-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 23:06:44 by mmanoel-          #+#    #+#             */
-/*   Updated: 2021/03/15 20:01:22 by mmanoel-         ###   ########.fr       */
+/*   Updated: 2021/03/15 22:16:50 by mmanoel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,23 @@
 
 size_t				ft_strlcat(char *dest, const char *src, size_t size)
 {
+	unsigned int	c;
+	unsigned int	c2;
 	unsigned int	c_dest;
 	unsigned int	c_src;
 
+	c = 0;
 	c_dest = ft_strlen(dest);
 	c_src = ft_strlen(src);
-	if (c_dest == size)
+	if (size <= c_dest)
 		return (size + c_src);
-	if (c_src < (size - c_dest))
-		ft_memcpy(dest + c_dest, src, c_src++);
-	else
-		ft_memcpy(dest + c_dest, src, size--);
-	dest[c_dest + size - 1] = '\0';
+	c2 = c_dest;
+	while (src[c] != '\0' && c2 != size - 1)
+	{
+		dest[c2] = src[c];
+		c++;
+		c2++;
+	}
+	dest[c2] = '\0';
 	return (c_dest + c_src);
 }
